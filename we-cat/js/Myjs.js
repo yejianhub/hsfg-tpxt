910f396ed9bf489be5de9507586df210bf87b2b6
Administrator <1653862780@qq.com>
2017-05-15 01:29:46 +0800

    yes

---
+++ we-cat/js/Myjs.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2017/5/11.
+ */
+$(function () {
+     $('.footer ul li a').click(function () {
+         $('.footer ul li').css('color','#f60');
+
+
+     })
+
+
+
+});
